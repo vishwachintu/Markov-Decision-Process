9674271670fa001851488b86b791422d2daab216
vish <32350453+vishwanathkavya@users.noreply.github.com>
2019-02-06 14:51:07 -0500

    Update MDP.py
    divided code policy

--- MDP.py
+++ MDP.py
@@ -103,6 +103,8 @@ def expected_utility(a, s, U, mdp):
     "The expected utility of doing a in state s, according to the MDP and U."
     return sum([p * U[s1] for (p, s1) in mdp.T(s, a)])
 
+"""----------------------------------------------------------------------------------------------""""
+#the below code is for the mdp policy
 
 def policy_iteration(mdp):
     "Solve an MDP by policy iteration [Fig. 17.7]"
